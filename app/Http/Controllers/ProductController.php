cb5798eeb8db38e6b5b5eedb7a2b3389c103c52b
simonscabello <simon.scabello@live.com>
2022-12-06 20:38:43 -0300

    Adiciona funcao para criar, listar e deletar produto.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
+use App\Models\Product;
+use App\Services\FileService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\UploadedFile;
 
 class ProductController extends Controller
 {
-    //
+    private FileService $fileService;
+
+    public function __construct()
+    {
+        $this->fileService = new FileService();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return response()->view('product/index', ['products' => Product::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create(): Response
+    {
+        return response()->view('product/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $file = $this->fileService->uploadFile($request, '/product');
+
+        Product::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'description' => $request->description,
+            'brand' => $request->brand,
+            'file_id' => $file->id
+        ]);
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Product $product
+     * @return RedirectResponse
+     */
+    public function destroy(Product $product): RedirectResponse
+    {
+        $product->delete();
+
+        return redirect()->route('product.index');
+    }
 }
